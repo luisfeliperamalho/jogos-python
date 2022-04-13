bedfcedd19b209a0c366158ef788fa070a33fb38
luisfeliperamalho <luis_felipero@hotmail.com>
2022-04-13 10:29:54 -0300

    Inclusão do jogo de adivinhacao

--- adivinhe_o_numero.py
+++ adivinhe_o_numero.py
@@ -2,18 +2,61 @@ print("********************************")
 print("Bem vindo no jogo de Adivinhação")
 print("********************************")
 
-numero_secreto = 87
+#condicao if e else
 
-chute_str = input("Escolhe um numero: ")
+# numero_secreto = 65;
 
-print("Você digitou o número", chute_str)
+# chute_str = input("Escolhe um numero: ");
 
-chute = int(chute_str)
+# print("Você digitou o número", chute_str);
 
-#condicao if else
-if numero_secreto != chute:
-    print("Você errou")
-else:
-    print("Você acertou")
+# chute = int(chute_str);
 
-print("Fim do jogo!")
+# acertou = chute == numero_secreto;
+# maior = chute > numero_secreto;
+# menor = chute < numero_secreto;
+
+# #condicao if else
+# if(acertou):
+#     print("Você acertou");
+# else:
+#     if(maior):
+#         print("Você errou! O seu chute foi maior do que o número secreto");
+#     elif (menor) :
+#         print("Você errou! O seu chute foi menor do que o número secreto");
+
+# print("Fim do jogo!");
+
+###############################################################################################################
+#condicao while
+
+#enquanto ainda há tentativas
+
+numero_secreto = 65;
+total_de_tentativas = 3;
+rodada = 1;
+
+while(rodada <= total_de_tentativas) :
+    print("Tentativa ", rodada, "de ", total_de_tentativas);
+    chute_str = input("Escolhe um numero: ");
+
+    print("Você digitou o número", chute_str);
+
+    chute = int(chute_str);
+
+    acertou = chute == numero_secreto;
+    maior = chute > numero_secreto;
+    menor = chute < numero_secreto;
+
+    #condicao if else
+    if(acertou):
+        print("Você acertou");
+    else:
+        if(maior):
+            print("Você errou! O seu chute foi maior do que o número secreto");
+        elif (menor) :
+            print("Você errou! O seu chute foi menor do que o número secreto");
+
+    rodada = rodada + 1;
+
+    print("Fim do jogo!");
